deab071bfd044acfea17459fd49cb6faae3bfdd2
GabrieleFederico <gabrielefederico@live.it>
2022-05-17 14:24:10 +0200

    Completed Challenge 2

---
+++ CreateWithCode/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float timePassed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timePassed < 2)
+            timePassed += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timePassed >= 2)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timePassed = 0;
+        }
+    }
+}
