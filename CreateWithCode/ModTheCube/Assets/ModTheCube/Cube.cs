aee3227f8a93e69a62e2076c9d91c680f6926d83
GabrieleFederico <gabrielefederico@live.it>
2022-05-17 17:24:58 +0200

    ModTheCube Challenge

---
+++ CreateWithCode/ModTheCube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+        
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * Random.Range(1.0f, 5f);
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(Random.Range(1.0f, 3f), 1.0f, 0.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
