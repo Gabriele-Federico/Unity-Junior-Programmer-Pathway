b380eb69410212e7d7467236848c5017527a99c2
GabrieleFederico <gabrielefederico@live.it>
2022-05-17 14:30:01 +0200

    Bonus Feature: Easy + added vertical bounds

--- CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,9 @@ public class PlayerController : MonoBehaviour
 {
 
     public GameObject projectilePrefab;
-    public float horizontalInput;
-    public float speed = 10f;
-    public float xRange = 10f;
+    private float horizontalInput, verticalInput;
+    private float speed = 10f;
+    private float xRange = 15f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +22,15 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
 
         if (transform.position.x < -xRange) transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-
         if (transform.position.x > xRange) transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
 
+
+        if (transform.position.z < -2) transform.position = new Vector3(transform.position.x, transform.position.y, -2);
+        if (transform.position.z > 17) transform.position = new Vector3(transform.position.x, transform.position.y, 17);
+
+        verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
     }
 }
