47326babd608caad41f9e61f4ac94d5b0816b62e
GabrieleFederico <80750645+Gabriele-Federico@users.noreply.github.com>
2022-05-21 14:26:50 +0200

    Finished Lesson 4.3

--- CreateWithCode2/Prototype4/Assets/Scripts/PlayerController.cs
+++ CreateWithCode2/Prototype4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerUp;
+    public float powerupStrenght = 15f;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,35 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxisRaw("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRb.AddForce(awayFromPlayer * powerupStrenght, ForceMode.Impulse);
+        }
+    }
+
+    private IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerupIndicator.SetActive(false);
     }
 }
